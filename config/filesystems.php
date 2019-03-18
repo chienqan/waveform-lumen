77d31ce63293bd8d6710c4b2e77112f2ff3d94ee
Chien Nguyen <gaamus@mail.com>
2019-03-18 15:30:00 +0700

    Create a binary facades

---
+++ config/filesystems.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chien
+ * Date: 2019-03-16
+ * Time: 16:52
+ */
---
+++ config/logging.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chien
+ * Date: 2019-03-18
+ * Time: 08:52
+ */
