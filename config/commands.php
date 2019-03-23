16d83fdbabea810ea80d41a4252b7128f813ca95
Chien Nguyen <gaamus@mail.com>
2019-03-23 19:15:03 +0700

    Put command list into foreach to reuse

---
+++ config/commands.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'wav2png' => [
+        'w',
+        'h',
+        'f',
+        'c',
+    ],
+
+    'imagemagick' => [
+
+    ],
+
+    'primitive' => [
+        'a',
+        'bg',
+        'm',
+        'n',
+        'nth',
+        'r',
+        'rep',
+        's',
+    ],
+];
